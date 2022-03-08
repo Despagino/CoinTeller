4f80993b2893015e758546d4eac0f3452c4d4cfa
Gino Tasis <gtasis84@gmail.com>
2022-03-07 21:15:14 -0500

    Created file structure, added my page title and connected to IBOutlet

---
+++ CoinTeller/Controller/CoinViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CoinViewController.swift
+//  CoinTeller
+//
+//  Created by Gino Tasis on 3/7/22.
+//
+
+import UIKit
+
+class CoinViewController: UIViewController {
+    
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
